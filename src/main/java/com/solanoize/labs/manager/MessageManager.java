b553ce8f163d9733ee577da2dae88560dc03cd7e
Solanoize <yanwarsolah@gmail.com>
2025-10-03 02:59:18 +0700

    clean unused package

--- src/main/java/com/solanoize/labs/manager/MessageManager.java
+++ src/main/java/com/solanoize/labs/manager/MessageManager.java
@@ -1,7 +1,6 @@
 package com.solanoize.labs.manager;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.solanoize.labs.service.EmailService;
